e5a8d4e948f7bbdbf10fb9ecc3db5c8b87157b37
杨仲谋 <johnson_yang@rainbowtech.net.cn>
2020-04-22 17:53:01 +0800

    create a branch

--- DemoApp/src/com/yzhmou/addServlet.java
+++ DemoApp/src/com/yzhmou/addServlet.java
@@ -13,7 +13,6 @@ public class addServlet extends HttpServlet{
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
 
 	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		
