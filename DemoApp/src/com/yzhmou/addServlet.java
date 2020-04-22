98713cbd3643fcf2fbf1bf1a872d904f26f04d5d
杨仲谋 <johnson_yang@rainbowtech.net.cn>
2020-04-22 17:34:59 +0800

    changes to addservelet

--- DemoApp/src/com/yzhmou/addServlet.java
+++ DemoApp/src/com/yzhmou/addServlet.java
@@ -10,6 +10,11 @@ import javax.servlet.http.HttpServletResponse;
 public class addServlet extends HttpServlet{
 	
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		
 		int i = Integer.parseInt(req.getParameter("num1")); 
